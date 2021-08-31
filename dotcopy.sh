31230c098c27caefea68d3e26f0a2dc5a8ebe4ab
Eklavya Sood <eklavyasood@gmail.con>
2021-08-31 16:49:50 +0530

    shell script to auto copy from .config

--- dotcopy.sh
+++ dotcopy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp -r /home/espc/.config/bspwm $HOME/Documents/Dotfiles
-cp -r /home/espc/.config/i3 $HOME/Documents/Dotfiles
-cp -r /home/espc/.config/kitty $HOME/Documents/Dotfiles
-cp -r /home/espc/.config/neofetch $HOME/Documents/Dotfiles
-cp -r /home/espc/.config/polybar $HOME/Documents/Dotfiles
-cp -r /home/espc/.config/rofi $HOME/Documents/Dotfiles
-cp -r /home/espc/.config/sxhkd $HOME/Documents/Dotfiles
+cp -r $HOME/.config/bspwm $HOME/Documents/Dotfiles
+cp -r $HOME/.config/i3 $HOME/Documents/Dotfiles
+cp -r $HOME/.config/kitty $HOME/Documents/Dotfiles
+cp -r $HOME/.config/neofetch $HOME/Documents/Dotfiles
+cp -r $HOME/.config/polybar $HOME/Documents/Dotfiles
+cp -r $HOME/.config/rofi $HOME/Documents/Dotfiles
+cp -r $HOME/.config/sxhkd $HOME/Documents/Dotfiles
