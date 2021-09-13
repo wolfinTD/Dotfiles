6357ecd9e0d8367c3d3d8ef65405c89be56e778c
Eklavya Sood <eklavyasood@gmail.con>
2021-09-13 23:29:18 +0530

    commit that is cool and good

--- dotcopy.sh
+++ dotcopy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cp -r $HOME/.config/awesome $HOME/Documents/Dotfiles
-cp -r $HOME/.config/bspwm $HOME/Documents/Dotfiles
-cp -r $HOME/.config/i3 $HOME/Documents/Dotfiles
-cp -r $HOME/.config/kitty $HOME/Documents/Dotfiles
-cp -r $HOME/.config/neofetch $HOME/Documents/Dotfiles
-cp -r $HOME/.config/polybar $HOME/Documents/Dotfiles
-cp -r $HOME/.config/rofi $HOME/Documents/Dotfiles
-cp -r $HOME/.config/sxhkd $HOME/Documents/Dotfiles
+cp -r $HOME/.config/awesome $HOME/Documents/Dotfiles/.config/
+cp -r $HOME/.config/bspwm $HOME/Documents/Dotfiles/.config
+cp -r $HOME/.config/i3 $HOME/Documents/Dotfiles/.config
+cp -r $HOME/.config/kitty $HOME/Documents/Dotfiles/.config
+#cp -r $HOME/.config/neofetch $HOME/Documents/Dotfiles/.config
+cp -r $HOME/.config/polybar $HOME/Documents/Dotfiles/.config
+cp -r $HOME/.config/rofi $HOME/Documents/Dotfiles/.config
+cp -r $HOME/.config/sxhkd $HOME/Documents/Dotfiles/.config
