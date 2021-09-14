05373da181046b73d7e34809935b816f951ba181
Eklavya Sood <eklavyasood@gmail.con>
2021-09-14 09:55:55 +0530

    comment

--- dotcopy.sh
+++ dotcopy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#this batch file copies config files for the programs i need in git, and pastes them into my git directory. it also copies this exact shell script from the git directory to my $HOME/.local/bin where it can be used as an executable from my run launcher
+
 cp -r $HOME/.config/awesome $HOME/Documents/Dotfiles/.config/
 cp -r $HOME/.config/bspwm $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/i3 $HOME/Documents/Dotfiles/.config
