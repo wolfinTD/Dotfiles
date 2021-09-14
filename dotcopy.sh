8c458bc95a156ead7d7163a5a11dea680e6a2674
Eklavya Sood <eklavyasood@gmail.con>
2021-09-14 09:56:34 +0530

    comment

--- dotcopy.sh
+++ dotcopy.sh
@@ -6,7 +6,6 @@ cp -r $HOME/.config/awesome $HOME/Documents/Dotfiles/.config/
 cp -r $HOME/.config/bspwm $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/i3 $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/kitty $HOME/Documents/Dotfiles/.config
-#cp -r $HOME/.config/neofetch $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/polybar $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/rofi $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/sxhkd $HOME/Documents/Dotfiles/.config
