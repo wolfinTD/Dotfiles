97d52bf7e319e6f0bd5ef6035e86532d9ca43b01
Eklavya Sood <eklavyasood@gmail.con>
2021-09-18 02:38:00 +0530

    spacevim added, other cleanups

---
+++ .config/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	#colorscript -e 34 | lolcat
+end
+
+#aliases to make life in the terminal easier for me
+alias nv="nvim"
+alias vim="nvim"
+alias vi="nvim"
+alias g="git"
+alias gits="git status"
+alias ga="git add"
+alias gau="git add -u"
+alias gc="git commit -m"
+alias gp="git push"
+alias ls="exa"
+alias gw2="./Downloads/GW2/play.sh"
+alias syu="sudo pacman -Syu"
+
+#exports to make defaults
+set -x EDITOR nvim
+set -x TERM alacritty
+set -x TERMINAL alacritty
+set -x VISUAL nvim
--- dotcopy.sh
+++ dotcopy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#this batch file copies config files for the programs i need in git, and pastes them into my git directory. it also copies this exact shell script from the git directory to my $HOME/.local/bin where it can be used as an executable from my run launcher
+# this batch file copies config files for the programs i need in git,
+# and pastes them into my git directory. 
 
 cp -r $HOME/.config/awesome $HOME/Documents/Dotfiles/.config/
 cp -r $HOME/.config/bspwm $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/i3 $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/kitty $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.config/fish $HOME/Documents/Dotfiles/.config
+cp -r $HOME/.config/fish/config.fish $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/polybar $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/rofi $HOME/Documents/Dotfiles/.config
+cp -r $HOME/.SpaceVim.d $HOME/Documents/Dotfiles/
 cp -r $HOME/.config/sxhkd $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/alacritty $HOME/Documents/Dotfiles/.config
 cp -r $HOME/.config/qtile $HOME/Documents/Dotfiles/.config
-cp    $HOME/Documents/Dotfiles/dotcopy.sh $HOME/.local/bin/
