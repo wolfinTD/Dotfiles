5c2672ecd1880965ae37abdf83615298a88620e5
ivan <eklavyasood@gmail.com>
2021-09-19 21:18:29 +0530

    switched to arch, changes

--- dotcopy.sh
+++ dotcopy.sh
@@ -3,14 +3,14 @@
 # this batch file copies config files for the programs i need in git,
 # and pastes them into my git directory. 
 
-cp -r $HOME/.config/awesome $HOME/Documents/Dotfiles/.config/
-cp -r $HOME/.config/bspwm $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.config/i3 $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.config/kitty $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.config/fish $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.config/polybar $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.config/rofi $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.SpaceVim.d $HOME/Documents/Dotfiles/
-cp -r $HOME/.config/sxhkd $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.config/alacritty $HOME/Documents/Dotfiles/.config
-cp -r $HOME/.config/qtile $HOME/Documents/Dotfiles/.config
+#cp -r $HOME/.config/awesome $HOME/docs/Dotfiles/.config/
+cp -r $HOME/.config/bspwm $HOME/docs/Dotfiles/.config
+#cp -r $HOME/.config/i3 $HOME/docs/Dotfiles/.config
+#cp -r $HOME/.config/kitty $HOME/docs/Dotfiles/.config
+cp -r $HOME/.config/fish $HOME/docs/Dotfiles/.config
+cp -r $HOME/.config/polybar $HOME/docs/Dotfiles/.config
+cp -r $HOME/.config/rofi $HOME/docs/Dotfiles/.config
+#cp -r $HOME/.SpaceVim.d $HOME/docs/Dotfiles/
+cp -r $HOME/.config/sxhkd $HOME/docs/Dotfiles/.config
+cp -r $HOME/.config/alacritty $HOME/docs/Dotfiles/.config
+#cp -r $HOME/.config/qtile $HOME/docs/Dotfiles/.config
