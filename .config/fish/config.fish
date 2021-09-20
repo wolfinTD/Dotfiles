6c1884a80bf7565171dd0050a5189a655bae6d06
ivan <eklavyasood@gmail.com>
2021-09-20 16:56:52 +0530

    doom stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,6 +21,7 @@ alias doomsync="/.emacs.d/bin/doom sync"
 alias doomdoctor="~/.emacs.d/bin/doom doctor"
 alias doomupgrade="~/.emacs.d/bin/doom upgrade"
 alias doompurge="~/.emacs.d/bin/doom purge"
+alias doombuild="~/.emacs.d/bin/doom build"
 
 #exports to make defaults
 set -x EDITOR nvim
--- dotcopy.sh
+++ dotcopy.sh
@@ -6,6 +6,7 @@
 #cp -r $HOME/.config/awesome $HOME/docs/Dotfiles/.config/
 cp -r $HOME/.bashrc $HOME/docs/Dotfiles
 cp -r $HOME/.config/bspwm $HOME/docs/Dotfiles/.config
+cp -r $HOME/.doom.d $HOME/docs/Dotfiles
 #cp -r $HOME/.config/i3 $HOME/docs/Dotfiles/.config
 #cp -r $HOME/.config/kitty $HOME/docs/Dotfiles/.config
 cp -r $HOME/.config/fish $HOME/docs/Dotfiles/.config
